add0ad3279a7cbb5cdb66afef9dd848826d8f356
linus <scubegames@gmail.com>
2020-03-31 14:38:31 +0200

    frontend

--- main.py
+++ main.py
@@ -3,19 +3,14 @@ from track import trackInfo
 
 app = Flask(__name__)
 
-@app.route('/app')
+@app.route('/')
 def appPage():
     data = trackInfo()
-    return render_template('app.html', 
-        tabTitle = 'What\'s playing?',
+    return render_template('app.html',
         art = data['albumArtwork'],
         track = data['title'],
         artist = data['artist'], 
         album = data['albumTitle'])
-
-@app.route('/settings')
-def settingsPage():
-    return render_template('settings.html')
 
 if __name__ == '__main__':
     app.run()
