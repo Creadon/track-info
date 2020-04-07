e4d55ccecc0719aa3d50130fca7511f1e1d3f4d5
linus <scubegames@gmail.com>
2020-04-07 18:47:04 +0200

    Added script to html

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from track import trackInfo
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def appPage():
-    data = trackInfo()
-    return render_template('index.html',
-        art = data['albumArtwork'],
-        track = data['title'],
-        artist = data['artist'], 
-        album = data['albumTitle'])
+    return render_template('index.html')
+
+@app.route('/title')
+def title():
+    return trackInfo()['title']
+
+@app.route('/artist') #TODO clean up returned list
+def artist():
+    return json.dumps(trackInfo()['artist'])
+
+@app.route('/album')
+def album():
+    return trackInfo()['albumTitle']
+
+@app.route('/artwork')
+def artwork():
+    return trackInfo()['albumArtwork']
 
 if __name__ == '__main__':
     app.run()
