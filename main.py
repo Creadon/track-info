32d0ccd1ef564198f7e124fcd90209c4487a3c16
linus <scubegames@gmail.com>
2020-04-08 17:11:57 +0200

    Merged main and track

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
+from netdisco.discovery import NetworkDiscovery
+from openhomedevice.Device import Device
 from flask import Flask, render_template, request
-from track import trackInfo
 import json
+
+#Device
+
+def ssdpDescription():
+    netdis = NetworkDiscovery()
+    netdis.scan()
+
+    for dev in netdis.discover():
+        data = json.loads(json.dumps(netdis.get_info(dev)))
+        if not '\'name\':' in str(data):
+            continue
+        name = data[0]['name']
+        if 'Linn Main Room' == name:
+            netdis.stop();
+            return data[0]['ssdp_description']
+
+    netdis.stop()
+    return 'Error'
+
+def trackInfo():
+    return json.loads(json.dumps(openhomeDevice.TrackInfo()))
+
+openhomeDevice = Device(ssdpDescription())
+
+#Server
 
 app = Flask(__name__)
 
